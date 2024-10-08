eecdf776f5668850c113901ef06b86324fdd0cfb
Hugo Chavar <hechavar@gmail.com>
2024-10-08 14:18:40 -0300

    Improve debug toggle formatting

--- src/components/DebugModeToggle.tsx
+++ src/components/DebugModeToggle.tsx
@@ -2,10 +2,11 @@ import { useEffect } from "react";
 
 interface Props {
   debugMode: boolean;
+  disabled: boolean;
   toggleDebugMode: () => void;
 }
 
-function DebugModeToggle({ debugMode, toggleDebugMode }: Props) {
+function DebugModeToggle({ debugMode, disabled, toggleDebugMode }: Props) {
   useEffect(() => {
     // Force re-render when debugMode changes
     if (debugMode) console.log("debugMode enabled");
@@ -13,10 +14,19 @@ function DebugModeToggle({ debugMode, toggleDebugMode }: Props) {
   }, [debugMode]);
 
   return (
-    <label>
-      <input type="checkbox" checked={debugMode} onChange={toggleDebugMode} />
-      Enable Debug Mode
-    </label>
+    <div className="form-check">
+      <input
+        className="form-check-input"
+        type="checkbox"
+        checked={debugMode}
+        onChange={toggleDebugMode}
+        id="debugCheck"
+        disabled={disabled}
+      />
+      <label className="form-check-label" htmlFor="debugCheck">
+        Enable Debug Mode
+      </label>
+    </div>
   );
 }
 
